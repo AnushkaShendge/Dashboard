b8cdf5bb4b8e5491846e5bee6da63e5bc15c2ab0
AnushkaShendge <anushkashendge5@gmail.com>
2024-10-23 17:18:33 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes , Route } from "react-router-dom"
 import Login from "./Pages/Login"
 import Register from "./Pages/Register"
-import Test from "./Pages/test"
+
 
 function App() {
 
@@ -9,7 +9,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/test" element={<Test />} />
     </Routes>
   )
 }
